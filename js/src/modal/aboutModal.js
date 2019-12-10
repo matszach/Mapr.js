ba0812e28fcfaf0edb98b49245bfb21ad2ebad27
matszach <lkaszubowski@gmail.com>
2019-12-10 23:56:12 +0100

    about section update

--- js/src/modal/aboutModal.js
+++ js/src/modal/aboutModal.js
@@ -6,6 +6,8 @@ const AboutModal = {
             <h1 class='modalTitle' >About</h1>
             <div class='modalTextBox'>
                 <p>Built with core JS and jQuery</p>
+                <p>Icons and images: <a href='https://game-icons.net/' target="_blank">Game-icons.net</a></p>
+                <br>
                 <p>Author: <a href='https://github.com/matszach' target="_blank">Matszach</a></p>
             </div>
         </div>
