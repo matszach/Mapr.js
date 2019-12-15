2267cc2d20a7e984ed470865bd38cb693206a16e
matszach <lkaszubowski@gmail.com>
2019-12-15 21:46:00 +0100

    palette editor modal view

---
+++ js/src/modal/colorsModal.js
@@ -0,0 +1,40 @@
+const ColorsModal = {
+
+    open(){
+        var html = `
+        <div id='modalMainDiv'>
+            <h1 class='modalTitle' >Colors</h1>
+            <div id='paletteDiv'></div>
+        </div>
+        `;
+        openModalWindow(html);
+        this.loadPalette();
+    },
+    
+    loadPalette(){
+        inputsHtml = '';
+        for(var i = 0; i < SettingsManager.colorPalette.colors.length; i++){
+            var color = SettingsManager.colorPalette.colors[i];
+            var html = `
+            <label class='paletteInputOverlay' id='paletteInputOverlay_${i}' style='background-color: ${color}'>
+                <p id='paletteText_${i}'>${color}</p>
+                <input type='color' class='paletteConfigHiddenInput' id='paletteInput_${i}' 
+                    value='${color}' onchange='ColorsModal.updateColor(${i})'/>
+            </label>`;
+            inputsHtml += html;
+            if(i % 6 == 5){
+                inputsHtml += '<br>';
+            }
+        }
+        $('#paletteDiv').html(inputsHtml);
+    },
+
+    updateColor(index){
+        var color = $(`#paletteInput_${index}`).val();
+        SettingsManager.colorPalette.colors[index] = color;
+        $(`#paletteInputOverlay_${index}`).css('background-color', color);
+        $(`#paletteText_${index}`).html(color);
+        MapEditor.currentTool.postLoad();
+    },
+    
+}
